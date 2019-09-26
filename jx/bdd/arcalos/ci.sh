19b176d80d08f13ea2f2c03d95e3c450d700158a
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-09-26 15:44:07 +0100

    fix: corrected bdd step

--- jx/bdd/arcalos/ci.sh
+++ jx/bdd/arcalos/ci.sh
@@ -47,7 +47,7 @@ helm repo add jenkins-x https://storage.googleapis.com/chartmuseum.jenkins-x.io
 jx step bdd \
     --use-revision \
     --version-repo-pr \
-    --versions-repo https://github.com/jenkins-x/arcalos-jenkins-x-versions.git \
+    --versions-repo https://github.com/cloudbees/arcalos-jenkins-x-versions.git \
     --config ../jx/bdd/arcalos/cluster.yaml \
     --gopath /tmp \
     --git-provider=github \
