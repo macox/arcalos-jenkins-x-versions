cb85ad07025d575a5a7dd7b6d82bf77ce03c2b47
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-10-03 16:48:19 +0100

    fix: attempting to set the PR SHA

--- ci.sh
+++ ci.sh
@@ -6,6 +6,7 @@ export VERSIONS_GIT_REF=$PULL_PULL_SHA
 
 git clone https://github.com/cloudbees/arcalos
 pushd arcalos
+  sed -i "s|VERSIONS_GIT_REF=.*$|VERSIONS_GIT_REF=$PULL_PULL_SHA|g" ./templates/.secrets.defaults
   ./create_aps_consumer_project.sh $NAME
   ./deploy_aps.sh $NAME
   ./verify.sh
